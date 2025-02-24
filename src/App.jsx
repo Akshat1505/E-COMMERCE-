e09cb10ed7c55898226fadfb7cb2518f341ba3cb
akshat <123@gmail.com>
2025-02-24 23:22:08 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Cart from './pages/Cart'
 import Product from './pages/Product'
 import PlaceOrder from './pages/PlaceOrder'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
@@ -27,6 +28,7 @@ const App = () => {
           <Route path='/orders' element={<Orders />}/>
 
         </Routes>
+        <Footer />
     </div>
   )
 }
