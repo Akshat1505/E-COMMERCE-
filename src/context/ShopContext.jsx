b9b89b23dbb9ad0b8df768e569b4012dd92bfe79
akshat <123@gmail.com>
2025-03-21 12:21:49 +0530

    update_3

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -9,7 +9,19 @@ const  currency = '$';
 const delivery_fee = 10;
 const [search , setSearch] = useState('');
 const [showSearch , setShowSearch] = useState(false);
+const [cartItems,setcartItems] = useState({});
 
+    const addToCart = async (itemId,size)=>{
+        let cartData = structuredClone(cartItems);
+
+        if(cartData(itemId)){
+            if(cartData[cartItems][size]){
+                cartData[cartItems][size]++;
+            }else{
+                cartData[cartItems][size] = 1;
+            }
+        }
+    }
 
     const value = {
         products , currency , delivery_fee,
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -2,12 +2,14 @@ import React, { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import { assets } from "../assets/assets";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
   const { products , currency} = useContext(ShopContext);
   const [productData, setproductData] = useState(false);
   const [image, setImage] = useState("");
+  const [size ,setSize] = useState('');
 
   const fetchProductData = async () => {
     products.map((item) => {
@@ -57,8 +59,37 @@ const Product = () => {
               </div>
               <p className="mt-5 text-3xl font-medium">{currency}{productData.price}</p>
               <p className="mt-5 text-gray-500 md:4/5">{productData.description}</p>
+              <div className="flex flex-col gap-4 my-8">
+                <p>Select Size</p>
+                <div className="flex gap-2">
+                  {productData.sizes.map((item,index)=>(
+                    <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item == size ? 'border-orange-500' : ''}`} key={index}>{item}</button>
+                  ))}
+                </div>
+              </div>
+              <button className="bg-black text-white px-8 py-3 text-sm active:bg-gray-700">Add To Cart</button>
+              <hr className="mt-8 sm:w-4/5" />
+              <div className="text-sm text-gray-500 mt-5 flex flex-col gap-1 ">
+                <p className="">100% Orignal Prodcut</p>
+                <p className="">Cash on Delivery</p>
+                <p className="">Easy Return and Exchange within 7 Days</p>
+              </div>
         </div>
       </div>
+      {/* ----Desc and Review Section------- */}
+      <div className="mt-20 ">
+        <div className="flex ">
+          <p className="border font-bold px-5 py-3 text-sm ">Description</p>
+          <p className="border px-5 py-3 text-sm">Reviews (122)</p>
+        </div>
+        <div className="flex flex-col gap-4 border px-6 text-sm text-gray-500">
+          <p>Testing</p>
+          <p>Testing___12</p>
+        </div>
+      </div>
+
+      {/* ----Display Related Products------ */}
+      <RelatedProducts  category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : (
     <div className="opacity-0"></div>
